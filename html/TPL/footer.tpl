48a2cbabebbd727607f43f2784419db9cadfcba3
Axel Francois <axel.francois-charlot@cpe.fr>
2022-03-25 16:30:07 +0100

    m

---
+++ html/TPL/footer.tpl
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="/CSS/header.css">
+<header>
+    <nav class="navbar">
+        <a href="/PHP/DISPLAY/accueil.php">
+            <img src="/MEDIA/img/logo.png" alt="logo" height="60" width="80"/>
+        </a>
+        <a href="/PHP/DISPLAY/bibliographie.php">
+            <img src="/MEDIA/img/book_sans_32.png" alt="logo">
+        </a>
+    </nav>
+</header>
--- html/index.php
+++ html/index.php
@@ -1,13 +1,11 @@
 <?php
     require_once('/usr/share/php/smarty3/Smarty.class.php');
-    echo "coucou";
     //Init connexion
     //require_once('PHP/CHECK_CO/init_connexion.php');
 
     // Init page d'accueil
     $smarty = new Smarty();
-
-    $smarty->assign('title', 'IHM - Robot');
-    //$smarty->assign('page', 'TPL/accueil.tpl');
+    
+    $smarty->assign('title', 'IHM-Robot');
     $smarty->display('TPL/main_structure.tpl');  
 ?>
