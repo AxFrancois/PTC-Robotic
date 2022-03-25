f9d9dbf60d1e2587d45a6a394101ea8b88b899ce
ElliotGarcia <elliotgarciabrewer@gmail.com>
2022-03-25 17:44:35 +0100

    Avancement style site et préparation php etc...

--- html/TPL/header.tpl
+++ html/TPL/header.tpl
@@ -1,6 +1,26 @@
 <link rel="stylesheet" href="/CSS/header.css">
 <header>
+    <div class="titre">
+        <h1>Robot</br></h1>
+        <p>Projet de Tronc Commun - 4 ETI</p>
+    </div>
     <nav class="navbar">
-
+        <ul>
+            <li>
+                <a href="#">
+                    Menu
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Debug
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Commandes
+                </a>
+            </li>
+        </ul>
     </nav>
 </header>
--- html/templates_c/11fc0993cdcd0e82a46a49e014e480dae07c9b6c_0.file.header.tpl.php
+++ html/templates_c/11fc0993cdcd0e82a46a49e014e480dae07c9b6c_0.file.header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2022-03-25 16:49:38
+/* Smarty version 3.1.39, created on 2022-03-25 17:11:35
   from '/var/www/html/TPL/header.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_623de492c147f8_78989025',
+  'unifunc' => 'content_623de9b7f26909_23300709',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '11fc0993cdcd0e82a46a49e014e480dae07c9b6c' => 
     array (
       0 => '/var/www/html/TPL/header.tpl',
-      1 => 1648223343,
+      1 => 1648224684,
       2 => 'file',
     ),
   ),
@@ -20,11 +20,31 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_623de492c147f8_78989025 (Smarty_Internal_Template $_smarty_tpl) {
+function content_623de9b7f26909_23300709 (Smarty_Internal_Template $_smarty_tpl) {
 ?><link rel="stylesheet" href="/CSS/header.css">
 <header>
+    <div class="titre">
+        <h1>Robot</br></h1>
+        <p>Projet de Tronc Commun - 4 ETI</p>
+    </div>
     <nav class="navbar">
-
+        <ul>
+            <li>
+                <a href="#">
+                    Menu
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Debug
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Commandes
+                </a>
+            </li>
+        </ul>
     </nav>
 </header><?php }
 }
