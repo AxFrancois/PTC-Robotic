ee2f7d98e76778c582e41662c4d28d9c5164f94b
AmauryCarre <72150319+AmauryCarre@users.noreply.github.com>
2022-05-21 17:21:14 +0200

    Update QrTest.py
    ça doit marcher inchallah

--- Python files/QrTest.py
+++ Python files/QrTest.py
@@ -2,35 +2,34 @@ import cv2
 from pyzbar.pyzbar import decode
 import time
 
-#-------cv2 init-----------------------------------
 
-cap = cv2.VideoCapture(0)
-cap.set(3,640) #largeur de la fenêtre
-cap.set(4,480) #hauteur
+def CodeReader():
+    cap = cv2.VideoCapture(0)
+    cap.set(3,640) #largeur de la fenêtre
+    cap.set(4,480) #hauteur
 
-#-------main---------------------------------------
+    used_codes=[]
+    camera = True
 
-used_codes=[]
-camera = True
+    while camera == True:
+        success, frame = cap.read()
 
-while camera == True:
-    success, frame = cap.read()
-    
-    for code in decode(frame):
-        #print('printing data')
-        print(code.data.decode('utf-8'))
-        if (code.data.decode('utf-8')) not in used_codes:
-            used_codes.append(code.data.decode('utf-8'))
-        #time.sleep(5)
-        camera = False
-        
-    cv2.imshow('Testing', frame)
-    cv2.waitKey(1)
+        for code in decode(frame):
+            #print('printing data')
+            res = code.data.decode('utf-8')
+            #print(res)
+            if (code.data.decode('utf-8')) not in used_codes:
+                used_codes.append(code.data.decode('utf-8'))
+            #time.sleep(5)
+            camera = False
 
-#-------end----------------------------------------
-cap.release()
-cv2.destroyAllWindows()
+        #cv2.imshow('Testing', frame)
+        #cv2.waitKey(1)
+    cap.release()
+    cv2.destroyAllWindows()
+    return(res)
 
+#CodeReader()
 
 
 
